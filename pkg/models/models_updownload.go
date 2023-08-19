02480ab6d078cdcd0c38ea1ebbcba975c3530301
oleve <oavelikanov@yandex.ru>
2023-08-19 05:04:44 +0300

    task 730 backend

--- pkg/models/models_updownload.go
+++ pkg/models/models_updownload.go
@@ -1,6 +1,11 @@
 package models
 
+type FileInfo struct {
+	Name  string `json:"file_name"`
+	IsDir bool   `json:"file_isdir"`
+}
+
 type FilesListDTO struct {
-	FilesList []string `json:"files_list"`
-	ServeURL  string   `json:"serve_url"`
+	FilesList []*FileInfo `json:"files_list"`
+	ServeURL  string      `json:"serve_url"`
 }
